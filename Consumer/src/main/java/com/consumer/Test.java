07142afad96e81468133d88b5e73c4d0a055578b
mahabir <mahabir7599@gmail.com>
2022-06-18 19:59:46 +0530

    consumer class added

---
+++ Consumer/src/main/java/com/consumer/Test.java
@@ -0,0 +1,13 @@
+package com.consumer;
+
+import java.util.function.Consumer;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Consumer<String> c = s->System.out.println(s);
+		c.accept("Mahwir");
+		c.accept("Bapi");
+	}
+
+}
